3a7a97d667cfe50831676080c1d82966ad825176
profbey <100024908+profbey@users.noreply.github.com>
2024-11-19 15:25:59 +0300

    small fixing

--- 4.pyOperators/btk4.1-atamaOperatorleri.py
+++ 4.pyOperators/btk4.1-atamaOperatorleri.py
@@ -9,20 +9,20 @@ x, y = y, x
 print(x,y,z)
 
 
-x += 5              # x = x+5  
-x -= 5              # x = x-5 
-x *= 5              # x = x*5
-x /= 5              # x = x/5
+x += 5                   # x = x+5  
+x -= 5                   # x = x-5 
+x *= 5                   # x = x*5
+x /= 5                   # x = x/5
 
-x %= 5              # x (mode 5)
-x //= 5             # x'i 5'e boler ama kalan c degerini verir.
+x %= 5                   # x (mode 5)
+x //= 5                  # x'i 5'e boler ama kalan c degerini verir.
 
-x **= 5             # x ust 5 => x^5
+x **= 5                  # x ust 5 => x^5
 
 print(x,y,z)
 
 
-values = 1,2,3      # veya => values = (1,2,3)
+values = 1,2,3           # veya => values = (1,2,3)
 
 x, y, z = values
 
@@ -30,8 +30,8 @@ print(x, y, z)
 
 
 values1 = 1,2,3,4,5,6
-x, y, *z = values1  # *z ifadesi z bir listeye donusur. output = 1 2 [3, 4, 5, 6]
-x, *y, z = values1  # output = 1 [2, 3, 4, 5] 6
+x, y, *z = values1       # *z ifadesi z bir listeye donusur. output = 1 2 [3, 4, 5, 6]
+x, *y, z = values1       # output = 1 [2, 3, 4, 5] 6
 
 print(x,y,z)
 
