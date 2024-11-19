423c4fc0ac0de94a045d8ac7d289ccbb3030aa72
profbey <100024908+profbey@users.noreply.github.com>
2024-11-19 20:35:53 +0300

    Create btk6.4-application.while.py

---
+++ 6.pyLoops/btk6.4-application.while.py
@@ -0,0 +1,19 @@
+numbers = [6,46,33,34,54,67,78,98,49]
+
+# 1- numbers listesini while ile ekrana yazdirin 
+
+# 2- baslangc v bitis degerlerini kullanicidan alip aradaki tum tek sayilari yazdirin 
+
+# 3- 1 ile 100 arasindaki degerleri tersten yazdirin
+
+# 4- kullanicidan aldiginiz 5 sayiyi buyukten kucuge dogru yazdirin
+
+# 5- kullanicidan alacagiz n tane urun bilgisini liste icinde saklayin
+#  **    urun sayisini kullaniciya sorun 
+#  **   dic list yapisi (name, price) seklinde olsun 
+#  **  urun ekleme islemi bittiginde whole ile listeleyin.
+
+
+
+
+
