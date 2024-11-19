cb363935e687c8bcd4a95fc4546a8a90c8c515b6
profbey <100024908+profbey@users.noreply.github.com>
2024-11-19 16:30:07 +0300

    Update btk4.3-comparisonMethods.py

--- 4.pyOperators/btk4.3-comparisonMethods.py
+++ 4.pyOperators/btk4.3-comparisonMethods.py
@@ -1,6 +1,6 @@
 # username, password => database
 
-# databaseden 'ahmet cavdar', '1234555'
+# databaseden 'ahmet bugday', '1234555'
 # dogru ise gris sagalanacak, bunun icin karsilastirma operatoru kullanilacak
 
 (a,b,c,d,f) = (55,45,45,15,54)
@@ -23,12 +23,4 @@ result = (c <= b)
 result = (True == 1)      # True
 result = (False == 0)     # True
 result = False + True +40 # output = 41
-
-
-
-
-
-
-
-
 
