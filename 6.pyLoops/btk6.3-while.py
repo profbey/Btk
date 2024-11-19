2069c3a7a990385f03245f4faa58e91f231dad15
profbey <100024908+profbey@users.noreply.github.com>
2024-11-19 15:26:47 +0300

    Create btk6.3-while.py

---
+++ 6.pyLoops/btk6.3-while.py
@@ -0,0 +1,16 @@
+x = 1           # numbers = btw 1-100
+
+while (x < 100) :
+        if (x %2 == 0 ):
+                print('only even numbers in range:',x)
+        else:
+                print('only odd numbers in range:',x)
+        x += 1
+
+
+name = ''       # False
+
+while not name.strip():
+        name = input('Name?: ')
+print(f'Wellcome, {name.title()}.')
+
