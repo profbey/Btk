3249a377e18f9ef909869d78c22ef3bba413b099
profbey <100024908+profbey@users.noreply.github.com>
2024-11-19 22:43:05 +0300

    Update btk6.4-application.while.py

--- 6.pyLoops/btk6.4-application.while.py
+++ 6.pyLoops/btk6.4-application.while.py
@@ -8,7 +8,32 @@ while (i < len(numbers)):
         print(numbers[i])
         i += 1
 
-# 2- baslangc v bitis degerlerini kullanicidan alip aradaki tum tek sayilari yazdirin 
+# 2- baslangc ve bitis degerlerini kullanicidan alip aradaki tum tek sayilari yazdirin 
+
+numbers = [6,46,33,34,54,7,78,9,49]
+
+
+try:
+        initialNumber = int(input('entry first number: '))
+        i = initialNumber
+        finalNumber = int(input('entry second number: '))
+        j = finalNumber
+
+        if finalNumber < numbers and numbers < initialNumber:
+                while j < i:
+                        j = j +1
+                        print(j)
+                
+        elif initialNumber < numbers and numbers < finalNumber :
+                while i < j:
+                        i = i +1
+                        print(i)
+        
+
+except ValueError:
+        print('try again')
+
+
 
 # 3- 1 ile 100 arasindaki degerleri tersten yazdirin
 
