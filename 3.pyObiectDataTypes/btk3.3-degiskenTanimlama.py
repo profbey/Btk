adc1f18225ded6e3c0b770f9196cc9c820f29a27
profbey <100024908+profbey@users.noreply.github.com>
2024-11-19 15:17:47 +0300

    small fixing

--- 3.pyObiectDataTypes/btk3.3-degiskenTanimlama.py
+++ 3.pyObiectDataTypes/btk3.3-degiskenTanimlama.py
@@ -5,10 +5,10 @@
 # Turkce karakter kullanmak yasak. Universal karakter kullanilmali 
 # Metinsel ifadeler " " veya ' ' icerisinde kullanilmali. " " ile ' ' arasinda islem onceligi var onun disinda pek fark yok.
 
-x = 1          # int
-y = 1.3        # float
+x = 1                   # int
+y = 1.3                 # float
 kullanici1 = "Adnan"    # str
-isStudent = True       # bool
+isStudent = True        # bool
 
 #veya bu sekilde tanimlanabilir. 
 
