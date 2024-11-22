c1ab437dc40d1c805fa9ba94d8bb53e7b7613bea
profbey <100024908+profbey@users.noreply.github.com>
2024-11-22 16:37:30 +0300

    Update btk6.8-application.guessNumber.py

---
+++ 6.pyLoops/btk6.8-application.guessNumber.py
@@ -0,0 +1,20 @@
+'''
+1 - 100 arasinda rastgele uretebilecek bir sayiyi asagi yukari ifadeleri ile buldurmaya calisin.
+
+** 'random' modulu icin 'python random' seklinde arama yapin.
+** 100 uzerinden puanlama yapin. her soru 20 puan.
+** hak bilgisini kullanicidan alin ve her soru belirtilen can sayisi uzerinden hesaplansin.
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
