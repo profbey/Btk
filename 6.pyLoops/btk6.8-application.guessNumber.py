dd87436aca2f2cbcf77c052c866215c7bda9fea2
profbey <100024908+profbey@users.noreply.github.com>
2024-11-24 03:09:53 +0300

    Update btk6.8-application.guessNumber.py

--- 6.pyLoops/btk6.8-application.guessNumber.py
+++ 6.pyLoops/btk6.8-application.guessNumber.py
@@ -9,27 +9,32 @@
 
 import random
 
-sayi = random.randint(1,100)
+sayi = random.randint(1,50)
 hak = 5
 sayac = 0
 
-while hak > 5:
-        hak -= 1
-        sayac +=1
-        tahmin = int(input('assumption : '))
+while hak <= 5:
+        try:
 
-        if sayi == tahmin:
-                print('congrat')
-                break
-        elif sayi > tahmin:
-                print('up')
-        else:
-                print('down')
+                tahmin = int(input('assumption : '))
+                hak -= 1
+                sayac +=1
+        
+
+                if sayi == tahmin:
+                        print('congrat')
+                        break
+                elif sayi > tahmin:
+                        print('up')
+                else:
+                        print('down')
 
 
-        if hak == 0 :
-                print(f'you lose. the assumption number is {sayi}')
-
+                if hak == 0 :
+                        print(f'you lose. the assumption number is {sayi}')
+                        break
+        except ValueError:
+                print('try again with numbers')
 
 
 
