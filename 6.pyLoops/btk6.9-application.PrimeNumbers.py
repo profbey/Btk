aaacb2a0de4e78bd6eba78d1e2aa7764f759f775
profbey <100024908+profbey@users.noreply.github.com>
2024-11-25 05:44:36 +0300

    Update btk6.9-application.PrimeNumbers.py

--- 6.pyLoops/btk6.9-application.PrimeNumbers.py
+++ 6.pyLoops/btk6.9-application.PrimeNumbers.py
@@ -1,19 +1,28 @@
-
 # Examples: Find whether an entered number is prime or not.
 
+    
+
+print('This app checks if the number is prime or not')
+from math import sqrt
+
+try:
+    number = int(input('Enter a number: '))
+    numberSqrt = int(sqrt(number))
+    primeNumber = True
+
+    for i in range(2, numberSqrt + 1):
+        if number % i == 0:
+            primeNumber = False
+            break
+        
+except ValueError:
+    print('try again with numbers.')
 
 
-
-
-
-
-
-
-
-
-
-
-
+if primeNumber:
+    print(f"The number {number} is prime.")
+else:
+    print(f"The number {number} is NOT prime.")
 
 
 
