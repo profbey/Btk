457f4994fb9feafc1e64d01de89f436d04c1f08b
profbey <100024908+profbey@users.noreply.github.com>
2024-11-28 02:20:49 +0300

    Update btk7.3-functionParameters.py

--- 7.pyFunction/btk7.3-functionParameters.py
+++ 7.pyFunction/btk7.3-functionParameters.py
@@ -8,8 +8,20 @@ print(cities)                           # Output = ['istanbul', 'izmir']
 
 
 
+def add(a,b,c = 0):                     # c = 0 , if c is not given, 0 is taken as default. 
+        return sum((a,b,c))             # This allows the function to work with 2 or 3 parameters.
 
 
+print(add(10,30))
+
+
+def add(*params):
+        print(params)                   # it gives the value as a tuple.
+        return sum(params)
+
+print(add(10,20,30))                    # Regardless of the other,
+print(add(10,20,30,40,50,60,70,70))     # the '*parameters' expression allows n assignments in this way.
+                                        
 
 
 
