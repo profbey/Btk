d1e226f82aa43ee44ff1bb8a5b4690af683d8501
profbey <100024908+profbey@users.noreply.github.com>
2024-11-28 02:55:25 +0300

    Update btk7.3-functionParameters.py

--- 7.pyFunction/btk7.3-functionParameters.py
+++ 7.pyFunction/btk7.3-functionParameters.py
@@ -15,8 +15,9 @@ def add(a,b,c = 0):                     # c = 0 , if c is not given, 0 is taken 
 print(add(10,30))
 
 
+
 def add(*params):
-        print(params)                   # it gives the value as a tuple.
+        print(params)                   # it gives the value as a tuple.(*tuple)
         return sum(params)
 
 print(add(10,20,30))                    # Regardless of the other,
@@ -24,9 +25,32 @@ print(add(10,20,30,40,50,60,70,70))     # the '*parameters' expression allows n 
                                         
 
 
+def add(*params):
+        summary = 0 
+        for i in params:                # its the alternative of above function.
+                summary = (summary + i)
+        return summary
 
 
 
+def userInfo(**args):                   # dic oldugunu belirtmek icin **args olarak girilir.(**dic)
+        for key,value in args.items():
+                print(f'{key} is {value}')
+        print('*** ' * 10 + '\n')
+     
+userInfo(name= 'ahmet'  , age = 22, city = 'new york')
+userInfo(name= 'mehmet' , age = 49, city = 'istanbul'   , phone = '+905445454545')
+userInfo(name= 'cengiz' , age = 65, city = 'malatya'    , phone = '+905655454545', email = 'cengiz77@gmail.com')
+
+
+
+def myFunction(a,b, *args, **kwargs):
+        print(a)                        # Output = 10
+        print(b)                        # Output = 20
+        print(args)                     # Output = (30, 40, 50, 60)
+        print(kwargs)                   # Output = {'key1': 'value 1', 'key2': 'value 2'}
+
+myFunction(10,20,30,40,50,60, key1 = 'value 1', key2 = 'value 2') 
 
 
 
