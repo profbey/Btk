6b081b3323add66a51f8b85febf148cb6c48e6c4
profbey <100024908+profbey@users.noreply.github.com>
2024-11-27 03:33:51 +0300

    Update btk7.3-functionParameters.py

---
+++ 7.pyFunction/btk7.3-functionParameters.py
@@ -0,0 +1,21 @@
+def change(n):
+        n[0] = 'istanbul'
+
+cities = ['ankara', 'izmir']
+ 
+change(cities)
+print(cities)                           # Output = ['istanbul', 'izmir']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
