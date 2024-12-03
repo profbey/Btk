62d2d4790f54f766f37d253fab0a8368a7d63e1a
profbey <100024908+profbey@users.noreply.github.com>
2024-12-03 04:56:03 +0300

    Update btk8.1-object-orientedProgramming-Definition.py

--- 8.pyObjectOrientedProgramming/btk8.1-object-orientedProgramming-Definition.py
+++ 8.pyObjectOrientedProgramming/btk8.1-object-orientedProgramming-Definition.py
@@ -4,21 +4,11 @@
 # instance (object)
 
 list1 = [1,2,3,4,5,6,7] 
-print(type(list1))
+print(type(list1))              # Output = <class 'list'>
 
+list1.append()                  # .append -> is an 'object' defined in the 'list' class.
 
+# CREATING A CLASS
+# class => Person(name,surname,age,ageCalculate()) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# age = userName.ageCalculate() -> dependency of class Person
