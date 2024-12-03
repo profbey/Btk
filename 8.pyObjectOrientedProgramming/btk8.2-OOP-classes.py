1a65189a96ecc9ed78b77b8b710696da0d0afa1a
profbey <100024908+profbey@users.noreply.github.com>
2024-12-03 04:56:11 +0300

    Update btk8.2-OOP-classes.py

---
+++ 8.pyObjectOrientedProgramming/btk8.2-OOP-classes.py
@@ -0,0 +1,23 @@
+# class
+class Person:
+        pass
+
+        # class attributes
+
+
+        # constructor (yapici methods)
+        def __init__(self, name, year ):
+                # object attributes
+                self.name = name
+                self.year = year
+
+
+                # methods  
+
+
+
+
+
+# object (instance)
+
+p1 = Person('mustafa', 1990)
