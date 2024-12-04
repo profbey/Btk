832644cbcb8fcc5a4f30a1b59cf09c2903f89f76
profbey <100024908+profbey@users.noreply.github.com>
2024-12-04 13:13:11 +0300

    Update btk8.3-OOP-methods.py

--- 8.pyObjectOrientedProgramming/btk8.3-OOP-methods.py
+++ 8.pyObjectOrientedProgramming/btk8.3-OOP-methods.py
@@ -1,45 +1,22 @@
-# class
-class Person:
-        pass
+class Circle:
+        # Class object attributes
+        pi = 3.14159265359
 
-        # class attributes
-        address = 'no info'
+        def __init__(self, radius = 1):
+                self.radius = radius
 
-        # constructor (yapici methods)
-        def __init__(self, name, year ):                        # The first index is self as stock. it is changeable
+        # methods
+        def surrounding(self):
+                return 2 * self.pi * self.radius
+                
+
+        def area(self):
+                return  self.pi * (self.radius ** 2)
                
+                
 
-                # object attributes
-                self.name = name
-                self.year = year
-                print('The init method is working **CheckPoint')
+c1 = Circle()   # r = 1
+c2 = Circle(3)  
 
-
-
-        # instance methods  
-        def ageCalculater(self):                 # Finding UserAge
-                """This calculate your birth 
-                ageCalculater = (Now - self.year) """
-                import datetime
-                nowYear = datetime.datetime.now().year
-                return nowYear - self.year
-        
-        def intro(self):
-                print(f'hello {self.name}, your age is {self.ageCalculater()}')
-
-# object (instance)
-p1 = Person('mustafa', 1990)
-p2 = Person(name = 'ramazan', year = 1996)
-
-# updates
-p1.name = 'ahmet'
-p1.address = 'Birmingham, UK'
-
-# accessing object attributes
-print(f'name: {p1.name}, birthday date: {p1.year} and address info: {p1.address}')
-print(f'name: {p2.name}, birthday date: {p2.year} and address info: {p2.address}')
-
-
-        
-
-p1.intro()
+print(f'circle1 area is {c1.area()}, circle surrounding is {c1.surrounding()}')
+print(f'circle2 area is {c2.area()}, circle surrounding is {c2.surrounding()}')
