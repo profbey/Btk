f9fbf62e6562250a952a9b6193002e3d861cba2d
profbey <100024908+profbey@users.noreply.github.com>
2024-12-04 12:07:55 +0300

    Update btk8.2-OOP-classes.py

--- 8.pyObjectOrientedProgramming/btk8.2-OOP-classes.py
+++ 8.pyObjectOrientedProgramming/btk8.2-OOP-classes.py
@@ -3,20 +3,29 @@ class Person:
         pass
 
         # class attributes
-
+        address = 'no info'
 
         # constructor (yapici methods)
-        def __init__(self, name, year ):
+        def __init__(self, name, year ):                        # The first index is self as stock. it is changeable
+               
+
                 # object attributes
                 self.name = name
                 self.year = year
-
+                print('The init method is working **CheckPoint')
 
                 # methods  
+                
+
 
 # object (instance)
 p1 = Person('mustafa', 1990)
+p2 = Person(name = 'ramazan', year = 1996)
 
-print(f'name: {p1.name} and birthday date: {p1.year}')
+# updates
+p1.name = 'ahmet'
+p1.address = 'Birmingham, UK'
 
-
+# accessing object attributes
+print(f'name: {p1.name}, birthday date: {p1.year} and address info: {p1.address}')
+print(f'name: {p2.name}, birthday date: {p2.year} and address info: {p2.address}')
