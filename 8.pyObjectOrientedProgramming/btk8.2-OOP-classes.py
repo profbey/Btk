cb8adcba869ebb21f604761b6034b69000ded286
profbey <100024908+profbey@users.noreply.github.com>
2024-12-04 13:05:23 +0300

    Update btk8.2-OOP-classes.py

--- 8.pyObjectOrientedProgramming/btk8.2-OOP-classes.py
+++ 8.pyObjectOrientedProgramming/btk8.2-OOP-classes.py
@@ -1,21 +1,30 @@
 # class
 class Person:
-        pass
-
+        
         # class attributes
         address = 'no info'
 
-        # constructor (yapici methods)
-        def __init__(self, name, year ):                        # The first index is self as stock. it is changeable
-               
+
+        # constructor ('yapici' methods)
+        def __init__(self, name, year ):                                # The first index is self as stock. it is changeable
 
                 # object attributes
                 self.name = name
                 self.year = year
-                print('The init method is working **CheckPoint')
+                print('The init method is working **CheckPoint')        # To control the system 
 
-                # methods  
-                
+
+        # instance methods  
+        def ageCalculater(self):                                        # Finding UserAge
+                """This calculate your birth 
+                ageCalculater = (Now - self.year) """
+                import datetime
+                nowYear = datetime.datetime.now().year
+                return nowYear - self.year
+        
+
+        def intro(self):
+                print(f'hello {self.name}, your age is {self.ageCalculater()}')
 
 
 # object (instance)
@@ -29,3 +38,6 @@ p1.address = 'Birmingham, UK'
 # accessing object attributes
 print(f'name: {p1.name}, birthday date: {p1.year} and address info: {p1.address}')
 print(f'name: {p2.name}, birthday date: {p2.year} and address info: {p2.address}')
+
+
+p1.intro()
