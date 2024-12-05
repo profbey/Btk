e152dc83aee3d14a742804f36a6818e748bedb5a
profbey <100024908+profbey@users.noreply.github.com>
2024-12-05 16:39:28 +0300

    Update btk8.6-OOP.Application-quizApp.py

--- 8.pyObjectOrientedProgramming/btk8.6-OOP.Application-quizApp.py
+++ 8.pyObjectOrientedProgramming/btk8.6-OOP.Application-quizApp.py
@@ -12,11 +12,12 @@ class Question():
 
 # QUIZ
 
-q1 = Question(f'What is the best programming language?', ['c++', 'python', 'javascript', 'html'], 'javascript')
-q2 = Question(f'What is the most popular programming language?', ['c++', 'python', 'javascript', 'html'], 'python')
+q1 = Question(f'What is the best programming language?'                 , ['c++', 'python', 'javascript', 'html'], 'javascript')
+q2 = Question(f'What is the most popular programming language?'         , ['c++', 'python', 'javascript', 'html'], 'python')
 q3 = Question(f'What is the the most money-making programming language?', ['c++', 'python', 'javascript', 'html'], 'c++')
 
 questions = [q1,q2,q3]
+
 
 class Quiz():
     def __init__(self, questions):
@@ -28,39 +29,45 @@ class Quiz():
         return self.questions[self.questionIndex]
 
     def displayQuestion(self):
-        question = self.getQuestion()                                  # Renamed 'questions' to 'question' for clarity
-        print(f'Question {self.questionIndex + 1}: {question.text}')   # Access the text attribute directly, not as a method call
+        question = self.getQuestion()                                   # Renamed 'questions' to 'question' for clarity
+        print(f'Question {self.questionIndex + 1}: {question.text}')    # Access the text attribute directly, not as a method call
 
 
         for q in question.choices:
             print(' ' + q)
 
         answer = input('** Answer: ')
+        self.guess(answer)                                              # Call self.guess() now that it's an instance method
+        self.loadQuestion()                                             # Call self.loadQuestion() now that it's an instance method
 
-        if question.checkAnswer(answer):                              # Call checkAnswer on the Question object (question)
-            print('Congrat!')
-        else: 
-            print(f'False Bro, the true answer is {question.answer}') # Corrected to question.answer
-        print('--- ' * 15 + '\n')
+        if question.checkAnswer(answer):                                # Call checkAnswer on the Question object (question)
+            self.score += 1 
+
+        print('- ' * 30 + '\n')
+
+    
+    def loadQuestion(self):
+        if len(self.questions) == self.questionIndex + 1:               # Changed to +1 to avoid index error
+            self.showScore()                                            # Corrected to call showScore instead of self.score()
+        else:
+            self.displayProgress()
+            self.questionIndex += 1 
+
+    def displayProgress(self):
+        totalQuestion = len(self.questions)
+        questionNumber= self.questionIndex + 1
+
+        if questionNumber > totalQuestion:
+            print('Quiz is over.')
+        else:
+            print(f' Question {questionNumber} of {totalQuestion} '.center(59,'*'))
+                                                                        # Define guess() and loadQuestion() as instance methods
+    def guess(self,answer):
+        question = self.getQuestion()
+
+    def showScore(self):
+        print(f'Score: ',self.score)
+
 
 quiz = Quiz(questions)
-quiz.displayQuestion()
-
-
-
-'''
-questions = quiz.questions[quiz.questionIndex]
-print(questions.text)
-'''
-
-
-
-
-'''print(q1.checkAnswer('javascript'))
-print(q2.checkAnswer('python'))
-print(q3.checkAnswer('python'))'''
-
-
-
-
-
+quiz.loadQuestion()
