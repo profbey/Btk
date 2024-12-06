6c0177f217003a58291ff12e5ea5bc90c6e2fa7e
profbey <100024908+profbey@users.noreply.github.com>
2024-12-06 22:03:07 +0300

    Update btk10.3-error_ Error Management.create Error Object.py

---
+++ 10.pyErrorManagment/btk10.3-error_ Error Management.create Error Object.py
@@ -0,0 +1,47 @@
+# Rasing an  Exception 
+
+'''
+
+when a memory is received, the 'except' statement already gives us an error.
+
+if we want to limit the function, we need to create our own error statement.
+in this case, the 'raise Exception()' statement is used
+
+'''
+
+# x = 10 
+# if x > 5:
+#         raise Exception('x cannot take value bigger than 5')
+
+def check_password(psw):
+       
+        import re
+
+        if len(psw) < 8:
+                raise Exception('Password must be minimum 8 character.')
+        elif len(psw) > 32:
+                raise Exception('Password must be maximum 32 character.')
+        elif not re.search('[a-z]', psw):
+                raise Exception('Password must contain at least one lower case letter.')
+        elif not re.search('[A-Z]', psw):
+                raise Exception('Password must contain at least one upper case letter.')
+        elif not re.search('[0-9]', psw):
+                raise Exception('Password must contain at least one number.')
+        elif not re.search('[_ @ # $ % &]', psw):
+                raise Exception('Password must contain at least one alpha numeric character.')
+        elif re.search(r'\s', psw):
+                raise Exception('Password cannot contain space')
+        else:
+                print('The password is accepted.')
+
+
+
+
+while True:
+        try:
+                password = input('Enter a valid password: ')
+                check_password(password)
+        except Exception as errorDisplay:
+                print(errorDisplay)
+        else:
+                break
