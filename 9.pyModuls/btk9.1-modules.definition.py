980220c59465757914ccd62ab93d77c6f442eadf
profbey <100024908+profbey@users.noreply.github.com>
2024-12-06 05:33:05 +0300

    Update btk9.1-modules.definition.py

---
+++ 9.pyModuls/btk9.1-modules.definition.py
@@ -0,0 +1,53 @@
+'''
+whats the modules?
+
+when doing a programming, there can be many complex.
+we should divide the program to easyly control the program and to update afterly.
+so, we use the modules structure.
+
+modules then communicate with each other.
+
+for example, module A can exchange and trigger with module b and module c as helpers.
+main.py is the structure that performs all the main triggers and operations.
+even if other modules perform operations with each other, they are eventually run in the main.py file as the main file.
+this is necessary both to keep track of how the program is working and for optimization.
+
+So instead of using a definition process or any other structure in the main file, we will only use the triggers of these modules.
+
+types of modules:
+
+1- you can prepare a module
+2- Existing modules
+        A) Standart Library Modules
+        B) Third part Modules   (pypi.org)
+                > pip install package-name
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
