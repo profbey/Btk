03fa9a12e8786fb034217ab4530f85683b64304b
profbey <100024908+profbey@users.noreply.github.com>
2024-12-06 07:20:23 +0300

    making 3rd part module

---
+++ 9.pyModuls/btk9.4-modules.preparing A Module/mod.py
@@ -0,0 +1,31 @@
+'''
+information about this module
+'''
+
+print('module was made')
+
+number = 10 
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+person = {
+        'name'          :'mustafa',
+        'birthdate'     : 1990,
+        'location'      : 'istanbul'
+}
+
+def func(x):
+        '''
+        the information of function
+        '''
+        print(f'x: {x}')
+
+class Person:
+        def processingDisplay():
+                print('The module is processing..')
+
+
+
+
+
+
