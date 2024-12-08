a09267ed7237fddf7f49907100c7268ac1dd33fc
profbey <100024908+profbey@users.noreply.github.com>
2024-12-08 23:36:26 +0300

    main structure of the application
    The main structure of the application has been created

---
+++ 11.pyFileManagment/btk11.5-fileManagment.application.File Managment/main.py
@@ -0,0 +1,38 @@
+class Note:
+        def __init__(self):
+                pass
+        
+        def islem():
+                pass
+        
+        def not_gir():
+                pass
+        
+        def ort_oku():
+                pass
+        
+        def save_notes():
+                pass
+        
+        def give_LetterGrade():
+                pass
+        
+        def save_quit():
+                pass
+        
+
+while True: 
+          islem = input('1) Showing Notes\n2) Enter Notes\n3) Save Notes\n4) Give Letter Grade and Export\n5) Quit\n')
+
+          if islem == 1:
+                  ort_oku()
+          elif islem == 2:
+                  not_gir()
+          elif islem == 3:
+                  save_notes()
+          elif islem == 4:
+                  give_LetterGrade()
+          elif islem == 5:
+                  save_quit()
+          else:
+                  pass
--- 11.pyFileManagment/btk11.5-fileManagment.application.File Managment/readme.txt
+++ 11.pyFileManagment/btk11.5-fileManagment.application.File Managment/readme.txt
@@ -2,6 +2,6 @@
 # gerektigi durumda bunu isleyecegiz        
 # puanlara gore bir harf notu gosterecegiz.
 # isim bilgisini ve not bilgisini bir dosya olarak yazdirip verecegiz. 
- 
+
 ## bir sonraki islemde not dagiliminin istatistiksel degerlerini
 ## bilgi ve grafiklerle yazdiracagiz
