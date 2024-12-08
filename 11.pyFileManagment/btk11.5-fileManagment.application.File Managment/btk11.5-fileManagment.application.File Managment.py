33947a6bd8b64ddbabe49102ff61123db8690221
profbey <100024908+profbey@users.noreply.github.com>
2024-12-08 23:07:28 +0300

    updating

---
+++ 11.pyFileManagment/btk11.5-fileManagment.application.File Managment/btk11.5-fileManagment.application.File Managment.py
@@ -0,0 +1,7 @@
+# kullanicidan isim bilgisi ve not bilgiisi istyecegiz. 
+# gerektigi durumda bunu isleyecegiz        
+# puanlara gore bir harf notu gosterecegiz.
+# isim bilgisini ve not bilgisini bir dosya olarak yazdirip verecegiz. 
+ 
+## bir sonraki islemde not dagiliminin istatistiksel degerlerini
+## bilgi ve grafiklerle yazdiracagiz
