48d4b8bd7799bf7206c32dccf7ce68cd03b13bfb
profbey <100024908+profbey@users.noreply.github.com>
2024-12-08 22:18:51 +0300

    Update btk11.3-fileManagment.reading File Function.py

---
+++ 11.pyFileManagment/btk11.3-fileManagment.reading File Function.py
@@ -0,0 +1,22 @@
+try:
+          file = open('C:/Users/ADNAN/Desktop/newFile1.txt','r',encoding='utf-8')
+          print(file)
+except FileNotFoundError:
+        print('the file doesnt exist')
+finally:
+          file.close()
+
+
+
+# with method -> islemin sonunda otomatik  olarak file.close() yapar.
+# open('dosya', 'okunmaBicimi') as file -> file = open('dosya', 'okunmaBicimi')
+
+with open('C:/Users/ADNAN/Desktop/newFile1.txt','r',encoding='utf-8') as file:
+        content = file.read()
+        print(content)
+
+        file.seek(0)          # .seek(index number) -> okuma islemi sonrasi curser'i index numarasina gonderir
+        print(file.tell())    # curser'in hangi konumda oldugunu soyler.
+        
+        content2 = file.read(10)
+        print(content2)
