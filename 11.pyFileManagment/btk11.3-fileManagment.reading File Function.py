e79ed8ddf98cd9631ca07f22b96df32da27d7797
profbey <100024908+profbey@users.noreply.github.com>
2024-12-08 22:58:10 +0300

    Update btk11.3-fileManagment.reading File Function.py

--- 11.pyFileManagment/btk11.3-fileManagment.reading File Function.py
+++ 11.pyFileManagment/btk11.3-fileManagment.reading File Function.py
@@ -1,5 +1,5 @@
 try:
-          file = open('C:/Users/ADNAN/Desktop/newFile1.txt','r',encoding='utf-8')
+          file = open('C:/Users/User/Desktop/newFile1.txt','r',encoding='utf-8')
           print(file)
 except FileNotFoundError:
         print('the file doesnt exist')
@@ -11,7 +11,7 @@ finally:
 # with method -> islemin sonunda otomatik  olarak file.close() yapar.
 # open('dosya', 'okunmaBicimi') as file -> file = open('dosya', 'okunmaBicimi')
 
-with open('C:/Users/ADNAN/Desktop/newFile1.txt','r',encoding='utf-8') as file:
+with open('C:/Users/User/Desktop/newFile1.txt','r',encoding='utf-8') as file:
         content = file.read()
         print(content)
 
