8476f906fcb39bad7db882146c8fd5a8cce21b65
profbey <100024908+profbey@users.noreply.github.com>
2024-12-13 05:26:14 +0300

    Update btk12.4-advancedFunctionFeatures.decoratorFunction.py

--- 12.pyAdvancedFunctionFeatures/btk12.4-advancedFunctionFeatures.decoratorFunction.py
+++ 12.pyAdvancedFunctionFeatures/btk12.4-advancedFunctionFeatures.decoratorFunction.py
@@ -1,29 +1,37 @@
+'''
+def my_decorater(func):
+    def wrapper():
+        print('before function')
+        func()
+        print('after function')
+    return wrapper
+
+def sayHello():
+    print('hello')
+
+def sayGreating():
+    print('greating')
+
+sayHello = my_decorater(sayHello)
+sayHello()
+
+sayGreating = my_decorater(sayGreating)
+sayGreating()
+'''
 
 
 
+def my_decorater(func):
+    def wrapper(name):
+        print('before function')
+        func(name)
+        print('after function')
+    return wrapper
 
+@my_decorater
+def sayHello(name):
+    print(f'hello {name}')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sayHello('user')
 
 
