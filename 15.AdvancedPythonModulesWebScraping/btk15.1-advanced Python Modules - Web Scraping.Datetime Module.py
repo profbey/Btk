1ec659c1431412f414c88f0b55a7c18003908fac
profbey <100024908+profbey@users.noreply.github.com>
2024-12-16 14:50:01 +0300

    Update btk15.1-advanced Python Modules - Web Scraping.Datetime Module.py

--- 15.AdvancedPythonModulesWebScraping/btk15.1-advanced Python Modules - Web Scraping.Datetime Module.py
+++ 15.AdvancedPythonModulesWebScraping/btk15.1-advanced Python Modules - Web Scraping.Datetime Module.py
@@ -3,6 +3,7 @@
 # from datetime import time
 
 from datetime import datetime  
+from datetime import timedelta
 now = datetime.now()
 
 
@@ -45,3 +46,32 @@ result = datetime.strftime(now,'%A')    # Monday
 print(result)
 
 
+
+t = '21 May 2024 hour 10:12:42'
+#day, month, year = t.split()
+
+dt = datetime.strptime(t, '%d %B %Y hour %H:%M:%S')     # 2024-05-21 10:12:42
+print(dt)
+
+result = dt.year
+print(result)                           # 2024
+
+
+
+birthday = datetime(1995,5,9,12,30,00)  # 1995-05-09 12:30:00
+print(birthday)                         
+
+result = datetime.timestamp(birthday)   # second             => 800011800.0
+result = datetime.fromtimestamp(result) # second to datetime => 1995-05-09 12:30:00
+print(result)                           
+
+
+result = now - birthday                 # timedelta => 10814 days, 0:00:49.476085
+#result = result.days
+#result = result.seconds
+#result = result.microsecond
+
+result += timedelta(days=16)            # timedelta => 10830 days, 0:00:49.476085
+result = now + timedelta(days=15)       # timedelta => 2024-12-31 12:39:19.164714
+
+print(result)
