473a7a0014c46b103612b9ab283782a3c85fb5a0
profbey <100024908+profbey@users.noreply.github.com>
2024-12-17 16:00:54 +0300

    Update btk11.1-fileManagment.opening File and Writing.py

--- 11.pyFileManagment/btk11.1-fileManagment.opening File and Writing.py
+++ 11.pyFileManagment/btk11.1-fileManagment.opening File and Writing.py
@@ -6,7 +6,7 @@
 # "w": (write) yazma modu. Dosyayi konumda olusturur.
 #         ** Dosyayi konumda olusturur.
 #         ** Dosya icerigin TAMAMINI siler ve yeniden ekleme yapar
-'''
+
 file = open('newFile.txt','w')
 file = open('C:/Users/User/Desktop/newFile.txt','w')
 file.close()
@@ -15,14 +15,14 @@ file.close()
 file = open('C:/Users/User/Desktop/newFile.txt','w',encoding='utf-8')
 file.write('Whatever you say here, it writes to the file.\n')
 file.close()
-'''
+
 
 # "a": (append) ekleme modu. Dosya konumda yoksa olusturur.
-'''
+
 file = open('C:/Users/User/Desktop/newFile.txt','a',encoding='utf-8')
 file.write('Whatever you say here, it appends to the file.\n')
 file.close()
-'''
+
 
 # "x": (create) olusturma modu. Dosya zaten varsa hata verir.
 file = open('C:/Users/User/Desktop/newFile2.txt','x',encoding='utf-8')
