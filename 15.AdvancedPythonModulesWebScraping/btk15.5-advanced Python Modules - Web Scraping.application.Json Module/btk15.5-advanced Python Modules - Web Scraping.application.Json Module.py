4a2df68a6f7f6884b64dcaea348f21a41022d706
profbey <100024908+profbey@users.noreply.github.com>
2024-12-17 18:53:05 +0300

    Create btk15.5-advanced Python Modules - Web Scraping.application.Json Module.py

---
+++ 15.AdvancedPythonModulesWebScraping/btk15.5-advanced Python Modules - Web Scraping.application.Json Module/btk15.5-advanced Python Modules - Web Scraping.application.Json Module.py
@@ -0,0 +1,55 @@
+# farkli platformlarda veri aktarimi ve kullaniminda onemlidir.
+
+import json
+
+
+while True:
+    print('Menu'.center(20,' '))
+
+    secim = input('1- Register\n2- Login\n3- Logout\n4- Identity\n5- Exit\n \nYour Choice?: '.center(20,' '))
+    if secim == '5':
+        break
+    else: 
+        if secim == '1':
+            #register
+            pass
+        elif secim == '2':
+            #login
+            pass
+        elif secim == '3':
+            #logout
+            pass
+        elif secim == '4':
+            #identity
+            pass
+        else:
+            False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
