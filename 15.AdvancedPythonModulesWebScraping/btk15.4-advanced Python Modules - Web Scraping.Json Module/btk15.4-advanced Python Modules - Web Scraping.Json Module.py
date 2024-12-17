a7351cbba1ceecc2879883c0c64ef773bd2a3db8
profbey <100024908+profbey@users.noreply.github.com>
2024-12-17 16:01:11 +0300

    Update btk15.4-advanced Python Modules - Web Scraping.Json Module.py

--- 15.AdvancedPythonModulesWebScraping/btk15.4-advanced Python Modules - Web Scraping.Json Module/btk15.4-advanced Python Modules - Web Scraping.Json Module.py
+++ 15.AdvancedPythonModulesWebScraping/btk15.4-advanced Python Modules - Web Scraping.Json Module/btk15.4-advanced Python Modules - Web Scraping.Json Module.py
@@ -2,10 +2,12 @@
 # dict yapisina benzerdir
 
 ########## Dict yapisi ########## 
-person = person_str = {
+person =  {
     "name"      :   "Ali",
     "languages" :   ["Python","C#"]
     }
+
+
 
 rst = person['name']
 rst = person['languages']
@@ -22,19 +24,29 @@ import json
 person_str = '{"name":"Ali", "languages":["Python","C#"]}'                  # string json bilgisi
 
 # JSON to Dict  ->  tek tirnak cift tirnak olayina dikkat et
-rst_str = json.loads(person_str)                                                # {'name': 'Ali', 'languages': ['Python', 'C#']}
+rst_str = json.loads(person_str)                                            # {'name': 'Ali', 'languages': ['Python', 'C#']}
 
 rst = rst_str['name']
-rst = rst_str['languages']
+rst = rst_str['languages'] 
 
 
 with open("person.json") as file:
-    data = json.loads(file)
+    data = json.load (file)
     print(data["name"])
     print(data["languages"])
 
 
+# Dict to JSON
 
+person_string = {"name":"Ali","languages":["Python","C#"]}
 
+result = json.dumps(person_string)
 
-print(rst)
+with open('person.json','w') as f:
+    json.dump(person_string,f)
+
+person_dict = json.loads(person_string)
+
+result = json.dumps(person_dict,indent=4 ,sort_keys=True  )
+
+print(result)
