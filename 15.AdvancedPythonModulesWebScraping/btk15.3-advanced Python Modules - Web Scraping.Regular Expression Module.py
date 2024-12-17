45b8cf12684c7b43081752e68d86d29a804f7cfb
profbey <100024908+profbey@users.noreply.github.com>
2024-12-17 08:31:48 +0300

    Update btk15.3-advanced Python Modules - Web Scraping.Regular Expression Module.py

--- 15.AdvancedPythonModulesWebScraping/btk15.3-advanced Python Modules - Web Scraping.Regular Expression Module.py
+++ 15.AdvancedPythonModulesWebScraping/btk15.3-advanced Python Modules - Web Scraping.Regular Expression Module.py
@@ -21,13 +21,11 @@ dir_re = [
 ]
 
 
+strEx= 'Python Course: "Zero To Hero" Guidline With Proffbey | 60+ hours'
+
 
 
 ###### re module
-
-strEx= 'Python Course: "Zero To Hero" Guidline With Profbey | 60+ hours'
-
-
 
 #* re.findall() method
 
@@ -61,3 +59,92 @@ rst = re.search('Course',strEx)         # <re.Match object; span=(7, 13), match=
 #rst = rst.group()                      # Course
 #rst = rst.string                       # Python Course: "Zero To Hero" Guidline With Profbey | 60+ hours
 
+
+###### regular expression 
+'''
+    [] -Koseli parantezler arasina yazilan butun karakterler aranir.
+
+        [abc]   =>  a       : 1 match
+                    ac      : 2 match
+                    Python  : No matches
+
+        [a-e]   =>  [abcde]
+        [1-5]   =>  [12345]
+        [0-39]  =>  [01239]
+
+        [^abc]  =>  abc disindaki karakterler
+        [^0-9]  =>  rakam olmayan karakterler
+'''
+rst = re.findall('[abc]',strEx)         # ['b']
+rst = re.findall('[hour]',strEx)        # ['h', 'o', 'o', 'u', 'r', 'r', 'o', 'o', 'r', 'o', 'u', 'h', 'r', 'o', 'h', 'o', 'u', 'r']
+rst = re.findall('[a-e]', strEx)        # ['e', 'e', 'e', 'd', 'e', 'b', 'e']
+rst = re.findall('[0-5]', strEx)        # ['0']
+
+rst = re.findall('[^abc]', strEx)       # ['P', 'y', 't', 'h', 'o', 'n', ' ', 'C', 'o', 'u', 'r', 's', 'e', ':', ' ', '"', 'Z', 'e', 'r',
+                                        #  'o', ' ', 'T', 'o', ' ', 'H', 'e', 'r', 'o', '"', ' ', 'G', 'u', 'i', 'd', 'l', 'i', 'n', 'e',
+                                        #  ' ', 'W', 'i', 't', 'h', ' ', 'P', 'r', 'o', 'f', 'e', 'y', ' ', '|', ' ', '6', '0', '+', ' ',
+                                        #  'h', 'o', 'u', 'r', 's']
+
+
+'''
+    .  -Herhangi bir tek karakteri belirtir
+
+        ..      =>  a       : No match
+                    ab      : 1 match
+                    abc     : 1 match
+                    abcd    : 2 match   
+'''
+rst = re.findall('...',strEx)           # ['Pyt', 'hon', ' Co', 'urs', 'e: ', '"Ze', 'ro ', 'To ', 'Her', 'o" ',
+                                        #  'Gui', 'dli', 'ne ', 'Wit', 'h P', 'rof', 'bey', ' | ', '60+', ' ho', 'urs']
+rst = re.findall('Py..on',strEx)        # ['Python']
+
+
+'''
+    ^  -strEX belirtilen karakter ile basliyor mu?
+
+        ^a      =>  a       : 1 match
+                    abc     : 1 match
+                    bac     : No match
+'''
+rst = re.findall('^a',strEx)            # []
+rst = re.findall('^P',strEx)            # ['P']
+
+
+'''
+    $  -strEX belirtilen karakter ile bitiyor mu?
+    
+        a$      =>  a       : 1 match
+                    lambda  : 1 match
+                    Python  : No match
+'''
+rst = re.findall('s$',strEx)            # ['s']
+rst = re.findall('t$',strEx)            # []
+rst = re.findall('hours$',strEx)        # ['hours']
+
+
+'''
+    *  -Bir karakterin sifir veya daha fazla sayida olup olmadigini
+        kontrol eder.
+    
+        ma*n    =>  mn      : 1 match
+                    man     : 1 match
+                    maaan   : 1 match
+                    main    : No match (a'nin bitisiginde n yok)
+'''
+rst = re.findall('co*r',strEx)          # []
+rst = re.findall('o*r',strEx)           # ['r', 'r', 'r', 'r', 'r']
+
+
+'''
+    +  -Bir karakterin sifir veya daha fazla sayida olup olmadigini
+        kontrol eder.
+    
+        ma+n    =>  mn      : No match
+                    man     : 1 match
+                    maaan   : 1 match
+                    main    : No match (a'nin bitisiginde n yok)
+'''
+rst = re.findall('o+u',strEx)           # ['ou', 'ou']
+
+
+print(rst)
